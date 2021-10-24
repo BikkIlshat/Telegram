80b99791ad2254d34f0aa4a12e2995c11651849a
BikkIlshat <HeavyIlshat@rambler.ru>
2021-10-24 12:24:14 +0300

    HW-7 Добавил Options menu с иконками

--- app/src/main/java/com/hfad/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hfad/telegram/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,18 @@
 package com.hfad.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
+import com.hfad.telegram.R
 import com.hfad.telegram.databinding.FragmentSettingsBinding
 
 class SettingsFragment : ViewBindingFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate){
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
